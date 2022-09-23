8b052a659d21b4450d2186cd689ea487234adcf3
jeonggu <jgkang9402@gmail.com>
2022-09-23 14:24:26 +0900

    api테스트

--- src/api/testApi.js
+++ src/api/testApi.js
@@ -1,5 +1,13 @@
+import axios from "axios";
+
 export default {
-	testFunc(param){
+	async testFunc(param){
 		console.log(param)
+		const result = await axios.get('https://api.odcloud.kr/api/15049995/v1/uddi:1a0d90fb-c631-4c81-93cb-9cf2f7bf968e?page=1&perPage=20&serviceKey=7aItO9jD4xiRXPLO4QdwPOc1T0MZmFQHgEMd7X1FjIjIJivJYGVrlY3Qu2k9eYiE%2F%2BTBPBMjvlQNob8ugu74%2FQ%3D%3D').then((response)=>{
+		// const result = await axios.get('https://7aItO9jD4xiRXPLO4QdwPOc1T0MZmFQHgEMd7X1FjIjIJivJYGVrlY3Qu2k9eYiE/+TBPBMjvlQNob8ugu74/Q==.odcloud.kr/oas/docs?namespace=15049995/v1/uddi:1a0d90fb-c631-4c81-93cb-9cf2f7bf968e').then((response)=>{
+			console.log(response.data)
+		// const result = axios.get('https://infuser.odcloud.kr/oas/docs?namespace=15049995/v1').then((response)=>{
+		})
+		console.log(result);
 	}
 };
